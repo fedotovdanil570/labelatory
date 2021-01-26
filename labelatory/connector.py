c7d11ccfdd39d17ffa2ed488f6b5abc7b2d0ca92
Daniil Fedotov <fedotovdanil570@gmail.com>
2021-01-27 01:38:49 +0400

    GitHubService, GitHubConnector, GitLabService, GitLabConnector implemented. Asynchronous communication implemented. Label checking added.

--- labelatory/connector.py
+++ labelatory/connector.py
@@ -1,47 +1,260 @@
 from abc import ABCMeta, abstractmethod
+import json
 import requests
 import aiohttp
 
 
 class DefaultConnector(metaclass=ABCMeta):
-    def __init__(self, user=None, repo=None, token=None):
-        self.user = user
-        self.repo = repo
+    def __init__(self, token=None, session=None):
+        # self.user = user
+        # self.repo = repo
         self.token = token
         headers = {
             'User-Agent': 'Labelatory',
             'Authorization': f'token {token}'
         }
-        self.session = aiohttp.ClientSession(headers=headers)
+        self.session = session # aiohttp.ClientSession(headers=headers)
 
     @abstractmethod
-    def get_labels(self, repo):
+    def get_labels(self, reposlug):
         """ Retrieves labels from repository. """
         raise NotImplementedError('Too generic. Use a subclass for getting labels.')
     
     @abstractmethod
-    def get_label(self, repo):
-        """ Retrieve label from repository. """
+    def get_label(self, reposlug, label):
+        """ Retrieve label with specified name from repository. """
         raise NotImplementedError('Too generic. Use a subclass for getting label.')
 
     @abstractmethod
-    def remove_label(self, repo):
+    def remove_label(self, reposlug, label):
         """ Removes label, which does not conform to configuration. """
         raise NotImplementedError('Too generic. Use a subclass for reverting the label.')
 
     @abstractmethod
-    def create_label(self, repo, label):
+    def create_label(self, reposlug, label):
         """ Creates new label in given repository. """
         raise NotImplementedError('Too generic. Use a subclass for creating a new label.')
 
     @abstractmethod
-    def update_label(self, repo, label):
+    def update_label(self, reposlug, label):
         """ Updates existing label in repository. """
         raise NotImplementedError('Too generic. Use a subclass for updating existing label.')
 
 
 class GitHubConnector(DefaultConnector):
-    pass
+    
+    API_ENDPOINT = 'https://api.github.com/'
 
+    def __init__(self, token=None, session=None):
+        super().__init__(token, session)
+        
+
+    async def get_labels(self, reposlug):
+        user, repo = reposlug.split('/')
+        URL = f'https://api.github.com/repos/{user}/{repo}/labels'
+        payload = {'per_page':100}
+
+        # Get first page of labels synchronously
+        headers = {
+            'User-Agent': 'Labelatory',
+            'Authorization': f'token {self.token}'
+        }
+        resp = requests.get(URL, headers=headers, params=payload)
+        page = resp.json()
+        if not resp.links.get('next'):
+            return (resp.status_code, page)
+        
+        # If there are more pages, get them in ansynchronous manner
+        while resp.links.get('next'):
+            URL = resp.links['next']['url']
+            async with self.session.get(URL) as resp:
+                page_content = await resp.json()
+                page.extend(page_content)
+
+        return (resp.status, page)
+
+    async def get_label(self, reposlug, label):
+        user, repo = reposlug.split('/')
+        URL = f'https://api.github.com/repos/{user}/{repo}/labels/{label.name}'
+
+        async with self.session.get(URL) as resp:
+            status = resp.status
+            label = await resp.json()
+        return (status, label)
+
+    async def create_label(self, reposlug, label):
+        user, repo = reposlug.split('/')
+        URL = f'https://api.github.com/repos/{user}/{repo}/labels'
+
+
+        data = {
+            'name': label.name, 
+            'color':label.color[1:], 
+            'description':label.description
+        }
+        # headers = {
+        #     'User-Agent': 'Labelatory',
+        #     'Authorization': f'token {self.token}'
+        # }
+        # resp = requests.post(URL, headers=headers, json=data)
+
+        # return (resp.status_code, resp.json())
+
+        async with self.session.post(URL, json=data) as resp:
+            resp_status = resp.status
+            resp_result = await resp.json()
+        return (resp_status, resp_result)
+
+    async def remove_label(self, reposlug, label):
+        user, repo = reposlug.split('/')
+        URL = f'https://api.github.com/repos/{user}/{repo}/labels/{label.name}'
+
+        # headers = {
+        #             'User-Agent': 'Labelatory',
+        #             'Authorization': f'token {self.token}'
+        # }
+        # resp = requests.delete(URL, headers=headers)
+        # return resp.status_code
+
+        async with self.session.delete(URL) as resp:
+            resp_status = resp.status
+        return resp_status
+
+    async def update_label(self, reposlug, label):
+        user, repo = reposlug.split('/')
+        URL = f'https://api.github.com/repos/{user}/{repo}/labels/{label._old_name}'
+
+        data = {
+            'new_name': label.name,
+            'color': label.color[1:],
+            'description': label.description
+        }
+
+        # headers = {
+        #     'User-Agent': 'Labelatory',
+        #     'Authorization': f'token {self.token}'
+        # }
+
+        # resp = requests.patch(URL, headers=headers, json=data)
+        # if resp.status_code == 200:
+        #     label._old_name = label.name
+        # return (resp.status_code, resp.json())
+
+        async with self.session.patch(URL, json=data) as resp:
+            resp_status = resp.status
+            resp_result = await resp.json()
+            if resp_status == 200:
+                label._old_name = label.name
+        return (resp_status, resp_result)
+        
+
+    
 class GitLabConnector(DefaultConnector):
-    pass
+    def __init__(self, host=None, token=None, session=None):
+        super().__init__(token, session)
+        if host:
+            self.host = host
+        else:
+            self.host = 'gitlab.com'
+    
+    async def get_labels(self, reposlug):
+        reposlug = reposlug.replace('/', '%2F')
+        URL = f'https://{self.host}/api/v4/projects/{reposlug}/labels'
+        payload = {'per_page':100}
+
+        # Get first page of labels synchronously
+        headers = {
+            'User-Agent': 'Labelatory',
+            'PRIVATE-TOKEN': f'{self.token}'
+        }
+        resp = requests.get(URL, headers=headers, params=payload)
+        page = resp.json()
+        if not resp.links.get('next'):
+            return (resp.status_code, page)
+        
+        # If there are more pages, get them in ansynchronous manner
+        while resp.links.get('next'):
+            URL = resp.links['next']['url']
+            async with self.session.get(URL) as resp:
+                page_content = await resp.json()
+                page.extend(page_content)
+
+        return (resp.status, page)
+
+    async def get_label(self, reposlug, label):
+        reposlug = reposlug.replace('/', '%2F')
+        URL = f'https://{self.host}/api/v4/projects/{reposlug}/labels/{label.name}'
+
+        async with self.session.get(URL) as resp:
+            status = resp.status
+            label = await resp.json()
+        return (status, label)
+
+    async def create_label(self, reposlug, label):
+        reposlug = reposlug.replace('/', '%2F')
+        URL = f'https://{self.host}/api/v4/projects/{reposlug}/labels'
+
+
+        data = {
+            'name': label.name, 
+            'color':label.color, 
+            'description':label.description
+        }
+        # headers = {
+        #     'User-Agent': 'Labelatory',
+        #     'Authorization': f'token {self.token}'
+        # }
+        # resp = requests.post(URL, headers=headers, json=data)
+
+        # return (resp.status_code, resp.json())
+
+        async with self.session.post(URL, json=data) as resp:
+            resp_status = resp.status
+            resp_result = await resp.json()
+        return (resp_status, resp_result)
+
+    async def remove_label(self, reposlug, label):
+        reposlug = reposlug.replace('/', '%2F')
+        URL = f'https://{self.host}/api/v4/projects/{reposlug}/labels/{label.name}'
+
+        # headers = {
+        #             'User-Agent': 'Labelatory',
+        #             'Authorization': f'token {self.token}'
+        # }
+        # resp = requests.delete(URL, headers=headers)
+        # return resp.status_code
+
+        async with self.session.delete(URL) as resp:
+            resp_status = resp.status
+        return resp_status
+
+    async def update_label(self, reposlug, label):
+        reposlug = reposlug.replace('/', '%2F')
+        URL = f'https://{self.host}/api/v4/projects/{reposlug}/labels/{label._old_name}'
+
+        data = {
+            'new_name': label.name,
+            'color': label.color,
+            'description': label.description
+        }
+
+        # headers = {
+        #     'User-Agent': 'Labelatory',
+        #     'Authorization': f'token {self.token}'
+        # }
+
+        # resp = requests.patch(URL, headers=headers, json=data)
+        # if resp.status_code == 200:
+        #     label._old_name = label.name
+        # return (resp.status_code, resp.json())
+
+        async with self.session.patch(URL, json=data) as resp:
+            resp_status = resp.status
+            resp_result = await resp.json()
+            if resp_status == 200:
+                label._old_name = label.name
+        return (resp_status, resp_result)
+
+    
+
+    
--- labelatory/labelatory.py
+++ labelatory/labelatory.py
@@ -1,6 +1,8 @@
 import pathlib
 import configparser
 import distutils.util
+import aiohttp
+import asyncio
 import requests
 import base64
 
@@ -37,14 +39,15 @@ class LabelatoryConfig():
         labels_rules - rules for labels;\
         source_secret - secret key for webhook to github repo, \
             where the labels configuration is stored. """
-    def __init__(self, services, labels_rules=None, source_secret=None):
+    def __init__(self, services=None, labels_rules=None, source_secret=None):
         self.services = services
         self.labels_rules = labels_rules
         self.source_secrete = source_secret
 
 class Label():
-    def __init__(self, type, color, description):
-        self.type = type
+    def __init__(self, name, color, description):
+        self.name = name
+        self._old_name = name
         self.color = color
         self.description = description
 
@@ -52,13 +55,18 @@ class Label():
     def load(cls, cfg_labels):
         pass
 
+class CheckResult():
+    def __init__(self, label_name, violations):
+        self.label_name = label_name
+        self.violations = violations
+
 class Service():
     def __init__(self, token, secret, repos):
         self.token = token
         self.secret = secret
-        repos = repos
+        self.repos = repos
         
-    def check(self):
+    def check(self, labels_rules):
         """ Checks whether all enabled repos of the service correctly defines labels. """
         raise NotImplementedError('Too generic. Use a subclass for check.')
 
@@ -66,15 +74,54 @@ class GitHubService(Service):
     def __init__(self, token, secret, repos, connector=None):
         super().__init__(token, secret, repos)
         if not connector:
-            self.connector = GitHubConnector()
+            self.connector = GitHubConnector(token)
         else:
             self.connector = connector
+        # self.repos = repos
+        self._headers = {
+            'User-Agent': 'Labelatory',
+            'PRIVATE-TOKEN': f'{self.token}'
+        }
 
-    def check(self):
-        pass
+    async def check(self, labels_rules):
+        async with aiohttp.ClientSession(headers=self._headers) as session:
+            self.connector.session = session
+
+            results = {}
+
+            for reposlug, enabled in self.repos.items():
+                if enabled:
+                    status, labels = await self.connector.get_labels(reposlug)
+                    if status != 200:
+                        raise Exception('Can\'t connect!')
+
+                    checked_labels = {}
+                    # Loop on every label in current repository
+                    for label in labels:
+                        violations = []
+                        name = label['name']
+                        label_rule = labels_rules.get(name)
+                        if label_rule:
+                            # Check label parameters. Raise an exception if wrong
+                            if label['color'] != label_rule.color[1:]:
+                                color = label['color']
+                                right_color = label_rule.color
+                                violations.append(f'Wrong label color in repository \'{reposlug}\' in label \'{name}\'. Must be \'{right_color}\', found \'{color}\'.')
+                                # raise Exception(f'Wrong label color in repository {reposlug} in label {name}. Must be {right_color}, found {color}.')
+                            if label['description'] != label_rule.description:
+                                description = label['description']
+                                right_description = label_rule.description
+                                violations.append(f'Wrong label description in repository \'{reposlug}\' in label \'{name}\'. Must be \'{right_description}\', found \'{description}\'.')
+                                # raise Exception(f'Wrong label description in repository {reposlug} in label {name}. Must be {right_description}, found {description}.')
+                        else:
+                            violations.append(f'Wrong label in repository \'{reposlug}\'. Label with name \'{name}\' is not defined.')
+                        checked_labels.update({name: violations})
+                    # print(f'Repo {reposlug} is OK :)')
+                    results.update({reposlug: checked_labels})
+            return results
 
     @classmethod
-    def load(cls, cfg_labels, token, secret, repos):
+    def load(cls, cfg, token, secret, repos):
         return GitHubService(
             token,
             secret,
@@ -91,12 +138,61 @@ class GitLabService(Service):
             self.host = host
 
         if not connector:
-            self.connector = GitLabConnector()
+            self.connector = GitLabConnector(host, token)
         else:
             self.connector = connector
+
+        self._headers = {
+            'User-Agent': 'Labelatory',
+            'Authorization': f'token {self.token}'
+        }
     
-    def check(self):
-        pass
+    async def check(self, labels_rules):
+        async with aiohttp.ClientSession(headers=self._headers) as session:
+            self.connector.session = session
+
+            results = {}
+
+            for reposlug, enabled in self.repos.items():
+                if enabled:
+                    status, labels = await self.connector.get_labels(reposlug)
+                    if status != 200:
+                        raise Exception(f'Can\'t connect! {status}')
+
+                    checked_labels = {}
+                    # Loop on every label in current repository
+                    for label in labels:
+                        violations = []
+                        name = label['name']
+                        label_rule = labels_rules.get(name)
+                        if label_rule:
+                            # Check label parameters. Raise an exception if wrong
+                            if label['color'] != label_rule.color:
+                                color = label['color']
+                                right_color = label_rule.color
+                                violations.append(f'Wrong label color in repository \'{reposlug}\' in label \'{name}\'. Must be \'{right_color}\', found \'{color}\'.')
+                                # raise Exception(f'Wrong label color in repository {reposlug} in label {name}. Must be {right_color}, found {color}.')
+                            if label['description'] != label_rule.description:
+                                description = label['description']
+                                right_description = label_rule.description
+                                violations.append(f'Wrong label description in repository \'{reposlug}\' in label \'{name}\'. Must be \'{right_description}\', found \'{description}\'.')
+                                # raise Exception(f'Wrong label description in repository {reposlug} in label {name}. Must be {right_description}, found {description}.')
+                        else:
+                            violations.append(f'Wrong label in repository \'{reposlug}\'. Label with name \'{name}\' is not defined.')
+                        checked_labels.update({name: violations})
+                    # print(f'Repo {reposlug} is OK :)')
+                    results.update({reposlug: checked_labels})
+            return results
+
+    @classmethod
+    def load(cls, cfg, token, secret, repos):
+        host = cfg.get('service:gitlab', 'host')
+        return GitLabService(
+            token,
+            secret,
+            repos,
+            host
+        )
 
 
 class ConfigLoader():
@@ -111,11 +207,11 @@ class ConfigLoader():
 
     @classmethod
     def _load_label_rule(cls, cfg_labels, label_rule):
-        label_type = label_rule[6:]
+        label_name = label_rule[6:]
         label_color = cfg_labels.get(label_rule, 'color')
         label_description = cfg_labels.get(label_rule, 'description')
         return Label(
-            label_type,
+            label_name,
             label_color,
             label_description
         )
@@ -133,7 +229,7 @@ class ConfigLoader():
             repo = validate_reposlug(repo)
             service_repos[repo] = bool(distutils.util.strtobool(status))
         # service_repos = {repo:bool(distutils.util.strtobool(enabled)) for repo, enabled in cfg_labels['repo:'+service_name].items()}#(s for s in cfg_labels.sections() if s.startswith('repo:'+service_name))
-        service = cls.SUPPORTED_SERVICES[service_name].load(cfg_labels, service_token, service_secret, service_repos)
+        service = cls.SUPPORTED_SERVICES[service_name].load(cfg, service_token, service_secret, service_repos)
         return service
 
     @classmethod
@@ -199,11 +295,10 @@ class ConfigLoader():
                 Configuration type can be only \'local\' or \'remote\'!')
 
         label_sections = (s for s in cfg_labels.sections() if s.startswith('label:'))
-
-        return LabelatoryConfig(
+        services=[cls._load_service(cfg, cfg_labels, service) for service in service_sections]
+        return services, LabelatoryConfig(
             # Loads every supported service
-            services=[cls._load_service(cfg, cfg_labels, service) for service in service_sections],
-            labels_rules=[cls._load_label_rule(cfg_labels, label_rule) for label_rule in label_sections],
+            labels_rules={label_rule[6:]: cls._load_label_rule(cfg_labels, label_rule) for label_rule in label_sections},
             source_secret=remote_secret
         )
 
@@ -216,7 +311,7 @@ def validate_reposlug(reposlug):
     return reposlug   
         
 
-def load_config(path):
+def load_app(path):
     try:
         cfg = configparser.ConfigParser()
         with open(path) as f:
@@ -230,5 +325,31 @@ def main():
     print('Hello!')
 
 if __name__ == "__main__":
-    config = load_config('credentials_conf.cfg')
-    print('End.')
+    services, config = load_app('credentials_conf.cfg')
+    # github = services.services[0]
+    # repos = github.repos
+    # label = Label('my_test_label', '#ffbf00', 'This is my first test label')
+    # for repo, enabled in repos.items():
+    #     if enabled:
+    #         github.connector.get_labels(repo)
+    #         _, j = github.connector.create_label(repo,  label)
+            
+    #         label.name = 'changed_name'
+    #         label.description = 'Changed description.'
+    #         s, j = github.connector.update_label(repo, label)
+    #         # s, j = github.connector.remove_label(repo, label)
+    # print(s, j)
+    async def _solve_tasks():
+        tasks = []
+        for service in services:
+            task = asyncio.ensure_future(service.check(config.labels_rules))
+            tasks.append(task)
+
+        result = await asyncio.gather(return_exceptions=True, *tasks)
+        from pprint import pprint
+        pprint(result)
+
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(_solve_tasks())
+    loop.close()

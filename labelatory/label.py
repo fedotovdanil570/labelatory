adc6c7076f19b089eef76077d2a23ae522372d71
Daniil Fedotov <fedotovdanil570@gmail.com>
2021-01-31 22:38:00 +0400

    Service class and all its inherited classes moved to services.py Violation class moved to label.py

--- labelatory/label.py
+++ labelatory/label.py
@@ -9,3 +9,11 @@ class Label():
     @classmethod
     def load(cls, cfg_labels):
         pass
+
+class Violation():
+    """ Holds the violation. Can be color, description or name """
+    def __init__(self, type, label, found=None, required=None):
+        self.label = label
+        self.type = type
+        self.required = required
+        self.found = found
